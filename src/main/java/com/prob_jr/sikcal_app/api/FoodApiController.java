b0321aa870d7b35772627d85c3e4503f7fba3c61
jeomxon <s_n_i_p_e_r_@naver.com>
2022-06-26 01:01:07 +0900

    Update Record && Add RecordFood

--- src/main/java/com/prob_jr/sikcal_app/api/FoodApiController.java
+++
@@ -1,8 +0,0 @@
-package com.prob_jr.sikcal_app.api;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-
-public class FoodApiController {
-}
--- src/main/java/com/prob_jr/sikcal_app/domain/Record.java
+++ src/main/java/com/prob_jr/sikcal_app/domain/Record.java
@@ -16,19 +16,10 @@ public class Record {
     @Column(name = "record_id")
     private Long id;
 
-    private LocalDateTime recordDate;
-
-    private int foodId;
-
-    private String foodName;
-
-    private int carbohydrate;
-    private int protein;
-    private int fat;
-    private int total_cal;
-
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    private LocalDateTime recordDate;
 
 }

f6ba5492baade2845bcdab96751809fe8560f0eb
jeomxon <jeomxon@gmail.com>
2022-07-20 23:13:41 +0900

    Change Record and Food Dto

---
+++ src/main/java/com/prob_jr/sikcal_app/domain/controller/dto/SaveFoodInfo.java
@@ -0,0 +1,11 @@
+package com.prob_jr.sikcal_app.domain.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SaveFoodInfo {
+
+    private Long recordFoodId;
+}
